47d88edd3fcef3936b49f3430532630a0190ba95
Daniel Sena <daniels_araujo@outlook.com.br>
2025-06-15 13:51:14 -0300

    05.09 - Melhorando o código

--- Projeto/main.js
+++ Projeto/main.js
@@ -1,14 +1,10 @@
 function tocaSom(idElementoAudio) {
     const elemento = document.querySelector(idElementoAudio);
 
-    if (elemento === null) {
-        console.log('Elemento não encontrado');
-    }
-
-    if (elemento != null) {
-        if (elemento.localName === 'audio') {
-            elemento.play();
-        }
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
     }
 }
 
